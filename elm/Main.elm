cc9fabf443487a3f5a85dc09ba2bbe659aee2272
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-01-10 20:59:39 +0100

    Move a square across the screen

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,10 +1,16 @@
 port module Main exposing (init)
 
+import Browser.Events
 import Platform
+import Time
+
+
+type alias Entity =
+    { id : String, x : Float, y : Float }
 
 
 type alias Model =
-    String
+    List Entity
 
 
 port setModel : (Model -> msg) -> Sub msg
@@ -13,26 +19,42 @@ port setModel : (Model -> msg) -> Sub msg
 port getModel : Model -> Cmd msg
 
 
+type alias Delta =
+    Float
+
+
 type Msg
     = SetModel Model
+    | Tick Delta
 
 
 init _ =
-    ( "test", Cmd.none )
+    let
+        initialModel =
+            [ { id = "square", x = 50, y = 50 } ]
+    in
+    ( initialModel, getModel initialModel )
 
 
-update msg _ =
+updateEntity delta entity =
+    { entity | x = entity.x + delta / 10 }
+
+
+update msg lastModel =
     let
         newModel =
             case msg of
                 SetModel model ->
                     model
+
+                Tick delta ->
+                    List.map (updateEntity delta) lastModel
     in
     ( newModel, getModel newModel )
 
 
 subscriptions model =
-    setModel SetModel
+    Sub.batch [ setModel SetModel, Browser.Events.onAnimationFrameDelta Tick ]
 
 
 main : Program () Model Msg
--- js/index.js
+++ js/index.js
@@ -1,32 +1,47 @@
-import { settings, Application, SCALE_MODES } from 'pixi.js'
+import * as PIXI from 'pixi.js'
 
-settings.SCALE_MODE = SCALE_MODES.NEAREST
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
 // settings.RESOLUTION = window.devicePixelRatio
-settings.RESOLUTION = 2
-const app = new Application({
+PIXI.settings.RESOLUTION = 2
+const app = new PIXI.Application({
   width: 800,
   height: 600,
-  backgroundColor: 0xffffff,
+  backgroundColor: 0x000000,
 })
 
 document.getElementById('game').appendChild(app.view)
 
 // app.loader.add('assets/spritesheet.json')
 
-app.loader.load(() => { 
-  console.log('pixi loaded!')
-})
-
-const elmApp = Elm.Main.init({
-  node: document.getElementById('elm')
-});
-
-const testAction = () => {
-  elmApp.ports.setModel.send('new message 2')
+const entity = {
+  
 }
 
-elmApp.ports.getModel.subscribe((model) => {
-  console.log('model logged: ', model);
+app.loader.load(() => { 
+  const graphics = new PIXI.Graphics()
+  entity.square = graphics
+  app.stage.addChild(graphics)
+  
+  console.log('pixi loaded!')
+
+  const elmApp = Elm.Main.init({
+    node: document.getElementById('elm')
+  });
+  
+  elmApp.ports.getModel.subscribe((model) => {
+    model.forEach(({id, x, y}) => {
+      entity[id]
+        .clear()
+        .beginFill(0xffffff)
+        .drawRect(x, y, 100, 100)
+        .endFill()
+    })
+  })
 })
 
-testAction()
+
+// const testAction = () => {
+//   elmApp.ports.setModel.send('new message 2')
+// }
+
+// testAction()
