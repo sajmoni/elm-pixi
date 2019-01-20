6e9cd0159a067d3c4d5d5aa8ac2516c15be24af1
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-01-20 13:17:13 +0100

    First version of rendering with Elm

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,8 +1,22 @@
-port module Main exposing (init)
+module Main exposing (init)
 
+import Browser
+import Browser.Dom exposing (getViewport)
 import Browser.Events
-import Platform
+import Color
+import Game.TwoD as Game
+import Game.TwoD.Camera as Camera exposing (Camera)
+import Game.TwoD.Render as Render exposing (Renderable)
+import Html exposing (Html, div)
+import Html.Attributes as Attr
+import Keyboard
+import Keyboard.Arrows
+import Task
 import Time
+
+
+
+-- MODEL
 
 
 type alias Entity =
@@ -10,50 +24,62 @@ type alias Entity =
 
 
 type alias Model =
-    List Entity
-
-
-
--- Incoming actions
-
-
-port updateSquare : (String -> msg) -> Sub msg
-
-
-
--- Outgoing subscriptions
-
-
-port updatePort : Model -> Cmd msg
-
-
-port initPort : Model -> Cmd msg
+    { entities : List Entity
+    , keys : List Keyboard.Key
+    , time : Float
+    , screen : ( Int, Int )
+    , camera : Camera
+    }
 
 
 type alias Delta =
     Float
 
 
+
+-- MSG
+
+
 type Msg
-    = UpdateSquare String
+    = ScreenSize Int Int
     | Tick Delta
+    | Keys Keyboard.Msg
 
 
-init : flags -> ( Model, Cmd msg )
+
+-- APP
+
+
+init : () -> ( Model, Cmd Msg )
 init _ =
     let
         initialModel =
-            [ Entity "square1" 50 50
-            , Entity "square2" 150 150
-            , Entity "square3" 250 250
-            ]
+            { entities =
+                [ Entity "square1" 50 50
+                , Entity "square2" 150 150
+                , Entity "square3" 250 250
+                , Entity "square4" 0 0
+                ]
+            , keys = []
+            , time = 0
+            , screen = ( 1000, 1000 )
+            , camera = Camera.fixedWidth 1000 ( 0, 0 )
+            }
     in
-    ( initialModel, initPort initialModel )
+    ( initialModel, Task.perform (\{ viewport } -> ScreenSize (round viewport.width) (round viewport.height)) getViewport )
 
 
-updateEntity : Delta -> Entity -> Entity
-updateEntity delta entity =
-    { entity | x = entity.x + delta / 10 }
+updateEntity : Delta -> List Keyboard.Key -> Entity -> Entity
+updateEntity delta keys entity =
+    let
+        arrows =
+            Keyboard.Arrows.arrows keys
+    in
+    if arrows.x > 0 then
+        resetEntity entity
+
+    else
+        { entity | x = entity.x + delta / 100 }
 
 
 resetEntity : Entity -> Entity
@@ -64,34 +90,62 @@ resetEntity entity =
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg lastModel =
     let
-        newModel =
-            case msg of
-                UpdateSquare idToUpdate ->
-                    List.map
-                        (\entity ->
-                            if entity.id == idToUpdate then
-                                resetEntity entity
-
-                            else
-                                entity
-                        )
-                        lastModel
-
-                Tick delta ->
-                    List.map (updateEntity delta) lastModel
+        _ =
+            Debug.log "lastModel" lastModel
     in
-    ( newModel, updatePort newModel )
+    case msg of
+        ScreenSize width height ->
+            ( { lastModel | screen = ( width, height ) }
+            , Cmd.none
+            )
+
+        Tick delta ->
+            ( { lastModel
+                | entities = List.map (updateEntity delta lastModel.keys) lastModel.entities
+                , time = lastModel.time + delta
+              }
+            , Cmd.none
+            )
+
+        Keys keyMsg ->
+            let
+                keys =
+                    Keyboard.update keyMsg lastModel.keys
+            in
+            ( { lastModel | keys = keys }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.batch [ updateSquare UpdateSquare, Browser.Events.onAnimationFrameDelta Tick ]
+    Sub.batch [ Browser.Events.onAnimationFrameDelta Tick ]
+
+
+
+--
+
+
+render : List Entity -> List Renderable
+render entities =
+    List.map (\e -> Render.shape Render.rectangle { size = ( 200, 200 ), color = Color.white, position = ( e.x, e.y ) }) entities
+
+
+view : Model -> Html msg
+view ({ time, screen } as model) =
+    div [ Attr.style "width" "100%", Attr.style "height" "1000px" ]
+        [ Game.render
+            { camera = model.camera
+            , time = time
+            , size = screen
+            }
+            (render model.entities)
+        ]
 
 
 main : Program () Model Msg
 main =
-    Platform.worker
+    Browser.element
         { init = init
         , update = update
+        , view = view
         , subscriptions = subscriptions
         }
--- js/index.js
+++ js/index.js
@@ -1,53 +1,8 @@
-import * as PIXI from 'pixi.js';
-
-PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
-// settings.RESOLUTION = window.devicePixelRatio
-PIXI.settings.RESOLUTION = 2;
-
-const app = new PIXI.Application({
-  height: 1136,
-  width: 640,
-  backgroundColor: 0x000000,
-});
-
-document.getElementById('game').appendChild(app.view);
-
-// app.loader.add('assets/spritesheet.json')
-
-const entity = {};
 
 const init = () => {
-  const elmApp = Elm.Main.init({
+  Elm.Main.init({
     node: document.getElementById('elm'),
-  });
-
-  const updateSquare = (id) => {
-    elmApp.ports.updateSquare.send(id);
-  };
-
-  elmApp.ports.initPort.subscribe((model) => {
-    model.forEach(({ id }) => {
-      const graphics = new PIXI.Graphics();
-      graphics.interactive = true;
-      graphics.on('mousedown', () => {
-        updateSquare(id);
-      });
-
-      app.stage.addChild(graphics);
-
-      entity[id] = graphics;
-    });
-  });
-
-  elmApp.ports.updatePort.subscribe((model) => {
-    model.forEach(({ id, x, y }) => {
-      entity[id]
-        .clear()
-        .beginFill(0xffffff)
-        .drawRect(x, y, 100, 100)
-        .endFill();
-    });
   });
 };
 
-app.loader.load(init);
+init();
