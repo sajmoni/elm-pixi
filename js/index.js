9390eedbb64f02a5133aa82392c37d1d148c1e01
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-01-20 11:32:23 +0100

    Add eslint and change resolution to iphone

--- js/index.js
+++ js/index.js
@@ -1,55 +1,53 @@
-import * as PIXI from 'pixi.js'
+import * as PIXI from 'pixi.js';
 
-PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 // settings.RESOLUTION = window.devicePixelRatio
-PIXI.settings.RESOLUTION = 2
+PIXI.settings.RESOLUTION = 2;
 
 const app = new PIXI.Application({
-  width: 800,
-  height: 600,
+  height: 1136,
+  width: 640,
   backgroundColor: 0x000000,
-})
+});
 
-document.getElementById('game').appendChild(app.view)
+document.getElementById('game').appendChild(app.view);
 
 // app.loader.add('assets/spritesheet.json')
 
-const entity = {}
+const entity = {};
 
-app.loader.load(() => { 
-  
+const init = () => {
   const elmApp = Elm.Main.init({
-    node: document.getElementById('elm')
-  })
-    
-  const updateSquare = ({id}) => {
-    elmApp.ports.updateSquare.send(id)
-  }
+    node: document.getElementById('elm'),
+  });
+
+  const updateSquare = (id) => {
+    elmApp.ports.incomingPort.send(id);
+  };
 
   elmApp.ports.initPort.subscribe((model) => {
-    model.forEach(({ id, x, y }) => {
-      const graphics = new PIXI.Graphics()
-      graphics.interactive = true
+    model.forEach(({ id }) => {
+      const graphics = new PIXI.Graphics();
+      graphics.interactive = true;
       graphics.on('mousedown', () => {
-        updateSquare({id})
-      })
+        updateSquare(id);
+      });
 
-      app.stage.addChild(graphics)
-      
-      entity[id] = graphics
-    })
-  })
-  
+      app.stage.addChild(graphics);
+
+      entity[id] = graphics;
+    });
+  });
+
   elmApp.ports.updatePort.subscribe((model) => {
-    model.forEach(({id, x, y}) => {
+    model.forEach(({ id, x, y }) => {
       entity[id]
         .clear()
         .beginFill(0xffffff)
         .drawRect(x, y, 100, 100)
-        .endFill()
-    })
-  })
-})
+        .endFill();
+    });
+  });
+};
 
-
-// testAction()
+app.loader.load(init);
